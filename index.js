518b629ffb052259b2d3975b773ffe13c5cd1beb
ant.pu <puhongwu@live.com>
2017-09-27 10:35:45 +0800

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const request = require('request')
+const http = require('http')
 const cheerio = require('cheerio')
 const fs = require('fs')
 
@@ -12,29 +12,36 @@ app.get('/', function(req, res) {
         if(err) {
             res.send("404")
         }
-        console.log(data);
         res.send(data.toString())
     })
 })
 
 app.get('/bing', function(req, res) {
     const keywords = req.query.q || ""
-    const url = encodeURI('http://www.bing.com/dict/search?q=' + keywords)
+    const url = encodeURI('http://cn.bing.com/dict/search?q=' + keywords)
 
-    request.get(url, function(_err, _res, data) {
-        var $ = cheerio.load(data, {
-            decodeEntities: false
+    http.get(url, (data) => {
+        var rawData = ''
+        
+        data.setEncoding('utf8');
+        data.on('data', function(chunk) {
+            rawData += chunk;
         })
 
-        var content = $('.contentPadding')
+        data.on('end', function() {
+         
+            var $ = cheerio.load(rawData, {
+                decodeEntities: false
+            })
 
-        const html = content.find('.qdef ul').html()
-        
-        res.send(html)
+            var content = $('.contentPadding')
+
+            const html = content.find('.qdef ul').html()
+            
+            res.send(html)
+        })
     })
 })
-
-
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
--- public/js/utils.js
+++ public/js/utils.js
@@ -93,6 +93,47 @@
 					}
 				})
 			})
+		},
+		debounce: function(fn, wait, immediate) {
+			let timeout
+			let args
+			let context
+			let timestamp
+			let result
+
+			const later = function later() {
+				const last = +(new Date()) - timestamp
+
+				if (last < wait && last >= 0) {
+					timeout = setTimeout(later, wait - last)
+				} else {
+					timeout = null
+					if (!immediate) {
+						result = fn.apply(context, args)
+						if (!timeout) {
+							context = args = null
+						}
+					}
+				}
+			}
+
+			return function debounced() {
+				context = this
+				args = arguments
+				timestamp = +(new Date())
+
+				const callNow = immediate && !timeout
+				if (!timeout) {
+					timeout = setTimeout(later, wait)
+				}
+
+				if (callNow) {
+					result = fn.apply(context, args)
+					context = args = null
+				}
+
+				return result
+			}
 		}
 	};
 })(window, jQuery);
